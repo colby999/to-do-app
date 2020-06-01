436f14ba6a968ce58d20a503a0b1b0099526827f
colby999 <60600699+colby999@users.noreply.github.com>
2020-06-01 13:01:15 +0200

    Add files via upload

---
+++ list/db_conn.php
@@ -0,0 +1,15 @@
+<?php 
+
+$sName = "localhost";
+$uName = "root";
+$pass = "root";
+$db_name = "todolist";
+
+try {
+    $conn = new PDO("mysql:host=$sName;dbname=$db_name", 
+                    $uName, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+  echo "Connection failed : ". $e->getMessage();
+}
+?>
